1767716c7f462ff872ddb59c31b8c466e37096d6
giraffe911 <407134358@qq.com>
2018-06-26 14:17:06 +0800

    podspect path to webAddress

---
+++ Example/Pods/podDemo/podDemo/Classes/ATBaseManager.h
@@ -0,0 +1,12 @@
+//
+//  ATBaseManager.h
+//  podDemo
+//
+//  Created by admin on 2018/6/26.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATBaseManager : NSObject
++ (void) printLog;
+@end
