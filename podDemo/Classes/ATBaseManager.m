f1a2863d19561d20bc1df50e020c4b96bafbaed8
giraffe911 <407134358@qq.com>
2018-06-26 13:29:52 +0800

    add 2 lib source files

---
+++ podDemo/Classes/ATBaseManager.m
@@ -0,0 +1,14 @@
+//
+//  ATBaseManager.m
+//  podDemo
+//
+//  Created by admin on 2018/6/26.
+//
+
+#import "ATBaseManager.h"
+
+@implementation ATBaseManager
++ (void) printLog {
+    NSLog(@"print log test!");
+}
+@end
