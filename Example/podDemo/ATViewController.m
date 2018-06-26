862878d2b5f6095abc14fd887cb7250ea7860dd5
giraffe911 <407134358@qq.com>
2018-06-26 13:29:23 +0800

    add lib files

--- Example/podDemo/ATViewController.m
+++ Example/podDemo/ATViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ATViewController.h"
+#import <podDemo/ATBaseManager.h>
 
 @interface ATViewController ()
 
@@ -18,6 +19,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [ATBaseManager printLog];
 }
 
 - (void)didReceiveMemoryWarning
